dbf32e3627772bb9bf67b2a85e7f3e2aa203986a
skpriya305-cmd <skpriya305@gmail.com>
2026-03-10 17:59:48 +0530

    Update GenericUtils.java

--- src/main/java/utils/GenericUtils.java
+++ src/main/java/utils/GenericUtils.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.slf4j.helpers.Reporter;
+import org.testng.Reporter;
 
 public class GenericUtils {
 	
